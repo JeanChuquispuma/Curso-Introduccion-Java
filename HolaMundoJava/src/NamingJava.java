7e552bae11cdbbca52e8971c96625978c81b9466
unknown <jdchuquispuma@gmail.com>
2022-06-14 09:59:26 -0500

    Convención de Nombres Java

---
+++ HolaMundoJava/src/NamingJava.java
@@ -0,0 +1,18 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 33333377;
+        int celPhone = 345643515;
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+
+        String currency = "MXN";
+        String background_color = "BLUE";
+
+        int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+    }
+}
