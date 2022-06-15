0ea3913568df0a3d4ba6cd12d802d8a5bdec9a3e
unknown <jdchuquispuma@gmail.com>
2022-06-15 06:05:12 -0500

    Sentencia Else

--- HolaMundoJava/src/ifStatement.java
+++ HolaMundoJava/src/ifStatement.java
@@ -1,12 +1,17 @@
 public class ifStatement {
     public static void main(String[] args) {
-        boolean isBluetoothEnabled = true;
+        boolean isBluetoothEnabled = false;
         int fileSended = 3;
 
         if (isBluetoothEnabled){
             //Send file
             fileSended++;
             System.out.println("Archivo enviado");
+        }else{
+            System.out.println("Por favor enciente tu bluetooth para iniciar la transferenia");
         }
+
+        System.out.println(isBluetoothEnabled);
+        System.out.println(fileSended);
     }
 }
